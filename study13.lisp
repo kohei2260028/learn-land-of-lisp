871990f3948b68dd0f7163cd85650312cbaff0a1
kohei2260028 <k.kusuhashi413@gmail.com>
2025-08-11 20:17:47 +0900

    Finished up to chapter 14

---
+++ study13.lisp
@@ -0,0 +1,7 @@
+(defun add-widget (database widget)
+  (cons widget database))
+(defparameter *database* nil)
+(defun main-loop ()
+  (loop (princ "Please enter the name of a new wideget")
+        (setf *database* (add-widget *database* (read)))
+        (format t "The database contains the following: ~a~%" *database*)))
