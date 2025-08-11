775ac75f3c8cc3f79fc5a95516f38e5c29fb3b91
kohei2260028 <k.kusuhashi413@gmail.com>
2025-08-11 17:37:38 +0900

    finished up to chap13

---
+++ study12.lisp
@@ -0,0 +1,8 @@
+(define-condition foo () ()
+  (:report (lambda (condition stream)
+             (princ "Stop Fooing around, numbskull" stream))))
+(defun bad-function ()
+  (error 'foo))
+(handler-case (bad-function)
+  (foo () "somebody signaled foo!")
+  (bar () "somebody signaled bar!"))
