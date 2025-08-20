9947f3510d297f92ffd0943234b5f0b469f3416e
kohei2260028 <k.kusuhashi413@gmail.com>
2025-08-21 01:06:52 +0900

    Finished up to chapter 16.

---
+++ study15.lisp
@@ -0,0 +1,21 @@
+(defmacro let1 (var val &body body)
+  `(let ((,var ,val))
+     ,@body))
+(defun add (a b)
+  (let1 x (+ a b)
+  (format t "The sum is ~a" x)
+  x))
+(defmacro split (val yes no)
+  (let1 g (gensym)
+   `(let1 ,g ,val
+      (if ,g
+          (let ((head (car ,g))
+                (tail (cdr ,g)))
+            ,yes)
+          ,no))))
+(defun my-length (lst)
+  (labels ((f (lst acc)
+             (split lst
+                    (f tail (1+ acc))
+                    acc)))
+    (f lst 0)))
